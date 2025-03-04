68a7f62f4c37e0415e4073ab94669eb88aaad740
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2025-03-04 04:29:13 +0100

    Style: import icons and add animtion
    icons are imported as standalong SVG files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import Sun from "../public/sun.svg"
+import Wind from "../public/wind.svg"
+import Rain from "../public/rain.svg"
+import Droplet from "../public/droplet.svg"
 
 function App() {
   
@@ -8,35 +12,39 @@ function App() {
     <>
       <main>
         <div className='header'>
-          <h3>right now in <span className='razzmatazz location'>Breda</span>, it's <span className='splash description'>mostly clear</span>.</h3>
+          <span>right now in <span className='razzmatazz location'>Breda</span>, it's <span className='splash description'>mostly clear</span>.</span>
           <div className="buttons">
             <button>Breda, NL</button><button>Adana, Turkey</button><button>Qinzhou, China</button>
           </div>
         </div>
         <section className="window">
           <div className='primary'>
-            <box-icon className="logo item" 
-              name='sun' 
-              type='solid' 
-              size="css"
-              ></box-icon>
+            <div className="logo item">
+              <img src={Sun} alt="" />
+            </div>
             <div className='temperature item'>
                 <span className='current'>66</span>
                 <span className='range'>61 / 69</span>
             </div>
             <div className='features item'>
               <div>
-                <box-icon className="logo" name='wind' size="css"></box-icon>
+                <div className="feature-logo">
+                  <img src={Wind} alt="" srcset="" />
+                </div>
                 <span className='current'>6</span>
                 <span>mph</span>
               </div>
               <div>
-                <box-icon className="logo" name='shower' size="css"></box-icon>
+                <div className="feature-logo">
+                  <img src={Rain} alt="" srcset="" />
+                </div>                
                 <span className='current'>0</span>
                 <span>%</span>
               </div>
               <div>
-                <box-icon className="logo" name='droplet' size="css"></box-icon>
+                <div className="feature-logo">
+                  <img src={Droplet} alt="" srcset="" />
+                </div>               
                 <span className='current'>30</span>
                 <span>%</span>
               </div>
